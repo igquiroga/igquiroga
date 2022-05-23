720a0257760963af4c89cbac15373a04a2bbb6c5
Ignacio Quiroga <ignacioquiroga94@gmail.com>
2022-05-23 17:20:29 -0300

    Start web page
    Starting my web page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>IGQUIROGA</title>
+	<link rel="stylesheet" type="text/css" href="Assets/css/bootstrap.min.css">
+</head>
+<body>
+
+	<?php require_once("Views/navbar.php"); ?>
+	<script src="Assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
